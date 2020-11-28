b825f2d053474c9fdf640e42b4f4991eba1e8fa1
SWAYAMSAIPANDA <73389521+SWAYAMSAIPANDA@users.noreply.github.com>
2020-11-28 17:57:05 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,12 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+      super(x,y,50,50);
+      this.image = loadImage("sprites/enemy.png");
+    }
+   
+  }
+  //parent class/ base class
+  //child class / sub - class
+  //child class inherits properties and functions from parent class
+  //BaseClass - pig, box, bird, log
+  
